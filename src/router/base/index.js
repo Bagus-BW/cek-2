5f898fb8f84843a75e562a8da75dd7df8124ce3a
Bagus-BW <bagusbudi.0701@gmail.com>
2023-10-28 14:59:53 +0700

    update: remove default layout on route lending

--- src/router/base/index.js
+++ src/router/base/index.js
@@ -1,12 +1,7 @@
-import DefaultLayout from "@/layouts/default";
-
 export default [
   {
     path: '/',
     name: 'landing',
     component: () => import('@/views/landing/index.vue'),
-    meta: {
-      layout: DefaultLayout
-    }
   }
 ]
