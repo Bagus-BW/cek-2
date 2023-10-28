ba88c70997e29ae19e415dc6587c87bbe373a52f
Bagus-BW <bagusbudi.0701@gmail.com>
2023-10-28 14:42:01 +0700

    add: directive click out side

--- src/core/global-plugins.js
+++ src/core/global-plugins.js
@@ -1,2 +1,7 @@
+import vueInit from '@/core/initiate.js';
+
+import VueDirective from "@/plugins/vue-directive.js";
+vueInit.use(VueDirective);
+
 import { register } from 'swiper/element/bundle';
 register();
