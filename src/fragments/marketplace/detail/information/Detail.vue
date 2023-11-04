3e447cfb090e42caaccfa10239f8db95120fa524
Bagus-BW <bagusbudi.0701@gmail.com>
2023-11-04 15:13:30 +0700

    update: responsive marketplace detail section detail

--- src/fragments/marketplace/detail/information/Detail.vue
+++ src/fragments/marketplace/detail/information/Detail.vue
@@ -6,66 +6,66 @@
     </div>
     <div class="flex flex-col mt-5 gap-3">
       <div class="grid grid-cols-12">
-        <div class="flex items-center col-span-4 border border-r-0 py-2 px-4 rounded-l-lg">
+        <div class="flex items-center col-span-12 md:col-span-4 border md:border-r-0 py-2 px-4 rounded-t-lg md:rounded-tr-none md:rounded-l-lg">
           <p class="text-base text-[#636363]">Tipe perjalanan</p>
         </div>
-        <div class="flex items-center col-span-8 border py-2 px-4 rounded-r-lg">
+        <div class="flex items-center col-span-12 md:col-span-8 border border-t-0 md:border-t py-2 px-4 rounded-b-lg md:rounded-bl-none md:rounded-r-lg">
           <p class="text-base font-bold text-black">Open Trip</p>
         </div>
       </div>
       <div class="grid grid-cols-12">
-        <div class="flex items-center col-span-4 border border-r-0 py-2 px-4 rounded-l-lg">
+        <div class="flex items-center col-span-12 md:col-span-4 border md:border-r-0 py-2 px-4 rounded-t-lg md:rounded-tr-none md:rounded-l-lg">
           <p class="text-base text-[#636363]">Meeting point</p>
         </div>
-        <div class="flex items-center col-span-8 border py-2 px-4 rounded-r-lg">
+        <div class="flex items-center col-span-12 md:col-span-8 border border-t-0 md:border-t py-2 px-4 rounded-b-lg md:rounded-bl-none md:rounded-r-lg">
           <p class="text-base font-bold text-black">Bandara Lampung</p>
         </div>
       </div>
       <div class="grid grid-cols-12">
-        <div class="flex items-center col-span-4 border border-r-0 py-2 px-4 rounded-l-lg">
+        <div class="flex items-center col-span-12 md:col-span-4 border md:border-r-0 py-2 px-4 rounded-t-lg md:rounded-tr-none md:rounded-l-lg">
           <p class="text-base text-[#636363]">Tanggal perjalanan</p>
         </div>
-        <div class="flex items-center col-span-8 border py-2 px-4 rounded-r-lg">
+        <div class="flex items-center col-span-12 md:col-span-8 border border-t-0 md:border-t py-2 px-4 rounded-b-lg md:rounded-bl-none md:rounded-r-lg">
           <p class="text-base font-bold text-black">Kamis, 2 Oktober 2023 - Selasa, 7 Oktober 2023</p>
         </div>
       </div>
       <div class="grid grid-cols-12">
-        <div class="flex items-center col-span-4 border border-r-0 py-2 px-4 rounded-l-lg">
+        <div class="flex items-center col-span-12 md:col-span-4 border md:border-r-0 py-2 px-4 rounded-t-lg md:rounded-tr-none md:rounded-l-lg">
           <p class="text-base text-[#636363]">Destinasi perjalanan</p>
         </div>
-        <div class="flex items-center col-span-8 border py-2 px-4 rounded-r-lg">
+        <div class="flex items-center col-span-12 md:col-span-8 border border-t-0 md:border-t py-2 px-4 rounded-b-lg md:rounded-bl-none md:rounded-r-lg">
           <p class="text-base font-bold text-black">Pulau Pahawang</p>
         </div>
       </div>
       <div class="grid grid-cols-12">
-        <div class="flex items-center col-span-4 border border-r-0 py-2 px-4 rounded-l-lg">
+        <div class="flex items-center col-span-12 md:col-span-4 border md:border-r-0 py-2 px-4 rounded-t-lg md:rounded-tr-none md:rounded-l-lg">
           <p class="text-base text-[#636363]">Kuota</p>
         </div>
-        <div class="flex items-center col-span-8 border py-2 px-4 rounded-r-lg">
+        <div class="flex items-center col-span-12 md:col-span-8 border border-t-0 md:border-t py-2 px-4 rounded-b-lg md:rounded-bl-none md:rounded-r-lg">
           <p class="text-base font-bold text-black">25 Orang</p>
         </div>
       </div>
       <div class="grid grid-cols-12">
-        <div class="flex items-center col-span-4 border border-r-0 py-2 px-4 rounded-l-lg">
+        <div class="flex items-center col-span-12 md:col-span-4 border md:border-r-0 py-2 px-4 rounded-t-lg md:rounded-tr-none md:rounded-l-lg">
           <p class="text-base text-[#636363]">Refund</p>
         </div>
-        <div class="flex items-center col-span-8 border py-2 px-4 rounded-r-lg">
+        <div class="flex items-center col-span-12 md:col-span-8 border border-t-0 md:border-t py-2 px-4 rounded-b-lg md:rounded-bl-none md:rounded-r-lg">
           <p class="text-base font-bold text-black">Paling lambat h-30 sebelum tanggal keberangkatan</p>
         </div>
       </div>
       <div class="grid grid-cols-12">
-        <div class="flex items-center col-span-4 border border-r-0 py-2 px-4 rounded-l-lg">
+        <div class="flex items-center col-span-12 md:col-span-4 border md:border-r-0 py-2 px-4 rounded-t-lg md:rounded-tr-none md:rounded-l-lg">
           <p class="text-base text-[#636363]">Reschedule</p>
         </div>
-        <div class="flex items-center col-span-8 border py-2 px-4 rounded-r-lg">
+        <div class="flex items-center col-span-12 md:col-span-8 border border-t-0 md:border-t py-2 px-4 rounded-b-lg md:rounded-bl-none md:rounded-r-lg">
           <p class="text-base font-bold text-black">Paling lambat 14 hari sebelum tanggal keberangkatan</p>
         </div>
       </div>
       <div class="grid grid-cols-12">
-        <div class="flex items-center col-span-4 border border-r-0 py-2 px-4 rounded-l-lg">
+        <div class="flex items-center col-span-12 md:col-span-4 border md:border-r-0 py-2 px-4 rounded-t-lg md:rounded-tr-none md:rounded-l-lg">
           <p class="text-base text-[#636363]">Keterangan kuota</p>
         </div>
-        <div class="flex items-center col-span-8 border py-2 px-4 rounded-r-lg">
+        <div class="flex items-center col-span-12 md:col-span-8 border border-t-0 md:border-t py-2 px-4 rounded-b-lg md:rounded-bl-none md:rounded-r-lg">
           <p class="text-base font-bold text-black">Tetap berangkat walaupun yang mendaftar tidak mencapai kuota yang tertera.</p>
         </div>
       </div>
