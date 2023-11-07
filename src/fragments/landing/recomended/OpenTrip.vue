c68e554c8c1e5415b9a2ece6710b66d24080b46e
Bagus-BW <bagusbudi.0701@gmail.com>
2023-11-08 06:51:49 +0700

    add: props id

--- src/fragments/landing/recomended/OpenTrip.vue
+++ src/fragments/landing/recomended/OpenTrip.vue
@@ -7,6 +7,7 @@
       <div class="grid grid-cols-12 gap-5 mt-5">
         <MiscCardContent
           v-for="i in 8"
+          :id="i"
           :key="i"
           class="col-span-12 md:col-span-4 lg:col-span-3"
           :with-discount="i === 1"
